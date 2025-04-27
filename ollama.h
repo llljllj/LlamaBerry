faa2cd885e98ebcda2c9bcbdf7a03b1d55d853cf
llljllj <1508032242@qq.com>
2025-04-27 11:45:23 +0800

    Update ollama.h

--- ollama.h
+++ ollama.h
@@ -11,7 +11,6 @@
 #include <QLabel>
 #include <QPixmap>
 #include <QTimer> 
-// 2. 在包含 Python 头文件前取消 slots 宏
 #ifdef slots
 #undef slots
 #endif
@@ -20,7 +19,6 @@ extern "C" {
     #include <Python.h>
 }
 
-// 3. 重新定义 slots 宏供 Qt 使用
 #define slots Q_SLOTS  // 恢复宏定义
 
 
@@ -50,7 +48,7 @@ private:
     QLabel *process_pic;
     QProcess process;
     QNetworkAccessManager *networkManager;
-    QStringList history;      // 保存多轮对话
-    QString lastUserInput;    // 上一次用户输入，发出去的问题
-    QString fullResponse;     // 当前AI回复，拼完整流式内容
+    QStringList history;     
+    QString lastUserInput;    
+    QString fullResponse;     
 };
